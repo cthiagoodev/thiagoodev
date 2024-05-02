5e91a63b6d0ef422cf8411e015105d02b385673a
thiagosousadev <thiagosousadev@gmail.com>
2024-05-02 17:01:06 -0300

    alteracoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:portfolio/firebase_options.dart';
 import 'package:portfolio/theme.dart';
 import 'package:portfolio/views/home_view.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
   ScreenUtil.ensureScreenSize();
   runApp(const MainApp());
 }
